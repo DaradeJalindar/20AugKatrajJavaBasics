066f02ee7753bcefeddb0fc636d35e8d96a6546f
velocity.ctc@gmail.com <velocity.ctc@gmail.com>
2022-09-09 09:11:10 +0530

    String concept

---
+++ src/basickeywords/MathematicalOperations.java
@@ -0,0 +1,26 @@
+package basickeywords;
+
+public class MathematicalOperations {
+	
+	public static void main(String[] args) {
+		
+		int a = 20;
+		
+		int b = 50;
+		 
+		int	c = a-b;
+		
+		System.out.println(c);//-30
+		
+		System.out.println(10+2);//12
+		
+//		Assignment:
+//		WAP to print the addition , substraction, mutiplication and division of 2 numbers
+		
+//														*				/
+		
+		
+			
+	}
+
+}
--- src/basickeywords/NonPrimitiveDataType.java
+++ src/basickeywords/NonPrimitiveDataType.java
@@ -2,4 +2,42 @@ package basickeywords;
 
 public class NonPrimitiveDataType {
 
+	
+	public static void main(String[] args) {
+		
+		String s = "Velocity";
+		
+		System.out.println(s);//Velocity
+		
+//		this is morning batch
+		System.out.println(s+"    corporate");//Velocity   corporate
+		
+		String s1 = "Training";
+		
+		System.out.println(s+" "+"corporate "+" "+s1);//Velocity corporate Training
+		
+		
+		int i = 50;
+		
+		
+		System.out.println(i+"a");//50a
+		
+		
+		System.out.println(i+"1");//501
+		
+		
+		System.out.println(20+2+"5");//225
+		
+		
+		System.out.println(20+2+"5"+2);//2252
+		
+		
+		
+	
+		
+		
+		
+		
+	}
+	
 }
