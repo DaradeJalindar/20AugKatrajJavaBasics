f6bad904cd3427fcdc0deab7d4933382ad3a5fa5
velocity.ctc@gmail.com <velocity.ctc@gmail.com>
2022-10-06 10:18:02 +0530

    overriding rules

--- src/polymorphism/A.java
+++ src/polymorphism/A.java
@@ -7,7 +7,25 @@ public class A {
 		System.out.println("m1 method from A Class");
 	}
 	
+	public int m2(int i)
+	{
+		System.out.println("m2 method with int argument");
+		return 60;
+	}
 	
+	
+	public static void m3()
+	{
+		System.out.println("static method m3 from A class");
+	}
+	
+	public void m4()
+	{
+		System.out.println("m4 method from A class");
+	}
+	
+	
+
 
 
 }
--- src/polymorphism/B.java
+++ src/polymorphism/B.java
@@ -7,6 +7,58 @@ public class B extends A {
 	{
 		System.out.println("m1 method from B Class");
 	}
+	
+	
+	public int m2(int i)
+	{
+		System.out.println("m2 method with int argument");
+		return 10;
+	}
+	
+	
+	public static void m3()
+	{
+		System.out.println("non static method m3 from B class");
+	}
+	
+	public void m4(String s)
+	{
+		System.out.println("m4 method of String argument from B class");
+	}
+	
+	public static void main(String[] args) {
+		
+		B b = new B();
+		
+		b.m1();// B class m1 method will get execute
+		b.m4();// A class m4 method method
+		
+		A a = new A();
+		
+		a.m1();// A class m1 method will get execute
+		
+		a.m4();// A class m4 method
+		
+		
+		A a1 = new B();
+		
+		a1.m1();// B class m1 method
+		
+		a.m1();// A class m1 method
+		
+		a.m2(50);  // A class m2 method will execute
+		
+		a1.m2(80); // B class m2 method
+		
+		a1.m4();  // A class m4 method
+		
+		
+		
+		
+		
+		
+		
+	}
 
 
 }
