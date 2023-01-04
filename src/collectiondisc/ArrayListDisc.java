4d0a580f5231eb51bea5f391cd0676e2382c5ca9
velocity.ctc@gmail.com <velocity.ctc@gmail.com>
2023-01-04 11:01:28 +0530

    Arraylist

---
+++ src/collectiondisc/ArrayListDisc.java
@@ -0,0 +1,32 @@
+package collectiondisc;
+
+import java.util.ArrayList;
+
+public class ArrayListDisc {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		ArrayList<String> al = new ArrayList<String>();
+		
+		al.add("def");
+		
+		al.add("abc");
+		
+		al.add("ghi");
+		
+		al.add("xyz");
+		
+		System.out.println(al);
+		
+		
+		int length = al.size();
+		
+		System.out.println(length);
+		
+
+		
+	}
+
+}
---
+++ src/collectiondisc/Test.java
@@ -0,0 +1,23 @@
+package collectiondisc;
+
+public class Test {
+	
+	
+	public static void main(String[] args) {
+		
+		int i [] = new int [5];
+		
+		i[0] = 50;
+		
+		i[1] = 0;
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
